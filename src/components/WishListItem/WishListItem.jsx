c8460bd9769583bfc1b7ffd7372d2d8dc4e62ad7
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-10-26 20:41:49 -0500

    Fixed a syntac error in wishList.jsx and on WishListItem.jsx I set up get request so i can get the currentCheapestCureentDeal for the wishListed game so I will then be able to update the WishListedGame with it.

--- src/components/WishListItem/WishListItem.jsx
+++ src/components/WishListItem/WishListItem.jsx
@@ -1,4 +1,22 @@
-const WishListItem = ({ gameName, thumbnail, CheapestCurrentDeal }) => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+const WishListItem = ({ gameName, thumbnail, CheapestCurrentDealId }) => {
+  const [cheapSharkGame, setCheapSharkGame] = useState();
+
+  const fetchCheapSharkGame = async () => {
+    try {
+      let response = await axios.get(
+        `https://www.cheapshark.com/api/1.0/games?title=${gameName}`
+      );
+
+      console.log(response);
+      setCheapSharkGame(response.data);
+    } catch (error) {}
+  };
+  CheapestCurrentDealId = response.data[0].cheapestDealID;
+  useEffect(() => {
+    fetchCheapSharkGame();
+  }, []);
   return (
     <div className="wishlistitem">
       <h2>{gameName}</h2>
--- src/components/Wishlist/WishList.jsx
+++ src/components/Wishlist/WishList.jsx
@@ -1,12 +1,5 @@
 import WishListItem from "../WishListItem/WishListItem";
 const WishList = ({ wishList = [] }) => {
-  /*   const [CheapestCurrentDealIdDetails, setCheapestCurrentDealIdDetails] =
-    useState();
-
-  useEffect(() => {
-    fetchCheapSharkApiDealDetails();
-  }, CheapestCurrentDealId);
- */
   const WishListItems = wishList.map((wishlistitem) => (
     <WishListItem
       key={wishlistitem.id}
