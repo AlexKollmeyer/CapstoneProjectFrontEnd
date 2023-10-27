35b6ba0bc63b9ffc96ca544759e8bdd4dbcd2aa8
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-10-27 12:39:40 -0500

    Moved the function useState and axios request for the cheapSharkGame to WishListPage to see if that will work better

--- src/components/WishListItem/WishListItem.jsx
+++ src/components/WishListItem/WishListItem.jsx
@@ -1,22 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 const WishListItem = ({ gameName, thumbnail, CheapestCurrentDealId }) => {
-  const [cheapSharkGame, setCheapSharkGame] = useState();
-
-  const fetchCheapSharkGame = async () => {
-    try {
-      let response = await axios.get(
-        `https://www.cheapshark.com/api/1.0/games?title=${gameName}`
-      );
-
-      console.log(response);
-      setCheapSharkGame(response.data);
-    } catch (error) {}
-  };
-  CheapestCurrentDealId = response.data[0].cheapestDealID;
-  useEffect(() => {
-    fetchCheapSharkGame();
-  }, []);
   return (
     <div className="wishlistitem">
       <h2>{gameName}</h2>
