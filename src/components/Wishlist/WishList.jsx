58526e5be5ea26a847b6596176e762c62a178577
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-10-28 13:16:05 -0500

    Made progress implementing the Sequential loading of the wishlist components

--- src/components/Wishlist/WishList.jsx
+++ src/components/Wishlist/WishList.jsx
@@ -1,13 +1,39 @@
 import WishListItem from "../WishListItem/WishListItem";
-const WishList = ({ wishList = [] }) => {
+const WishList = ({ wishList = [], wishListLoaded }) => {
   const WishListItems = wishList.map((wishlistitem) => (
     <WishListItem
       key={wishlistitem.id}
       gameName={wishlistitem.gameName}
       thumbnail={wishlistitem.thumbnail}
+      wishListLoaded={wishListLoaded}
     />
   ));
-  return <div>{WishListItems}</div>;
+  return <div>{wishListLoaded ? WishListItems : <p>Loading</p>}</div>;
 };
 
 export default WishList;
+/* import React, { useState, useEffect } from 'react';
+
+const ComponentA = () => {
+  const [componentALoaded, setComponentALoaded] = useState(false);
+
+  // Simulate loading ComponentA
+  useEffect(() => {
+    // Simulate an API call or any async operation
+    setTimeout(() => {
+      setComponentALoaded(true);
+    }, 2000); // Simulating a 2-second loading time
+  }, []);
+
+  return (
+    <div>
+      {componentALoaded ? <ComponentB /> : <p>Loading Component A...</p>}
+    </div>
+  );
+};
+
+const ComponentB = () => {
+  return (
+    <div>
+      <p>Component B Loaded!</p>
+      {/* Your Component B content goes here */
--- src/pages/WishListPage/WishListPage/WishListPage.jsx
+++ src/pages/WishListPage/WishListPage/WishListPage.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 const WishListPage = ({}) => {
   const [user, token] = useAuth();
   const [wishList, setWishList] = useState([]);
+  const [wishListLoaded, setWishListLoaded] = useState(false);
   const fetchWishList = async () => {
     try {
       let response = await axios.get(
@@ -25,10 +26,14 @@ const WishListPage = ({}) => {
   };
   useEffect(() => {
     fetchWishList();
+    setTimeout(() => {
+      setWishListLoaded(true);
+    }, 1500);
   }, []);
+
   return (
     <div>
-      <WishList wishList={wishList} />
+      <WishList wishList={wishList} wishListLoaded={wishListLoaded} />
     </div>
   );
 };
