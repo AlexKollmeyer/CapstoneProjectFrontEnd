be8a816bcb88700645e187515b80266f5eb08c0e
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-11-01 12:22:11 -0500

    Finished the framework for the Browsing page itself

--- src/pages/BrowsingPage/BrowsingPage.jsx
+++ src/pages/BrowsingPage/BrowsingPage.jsx
@@ -4,7 +4,30 @@ import { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
 import axios from "axios";
 const BrowsingPage = ({}) => {
-  return <div></div>;
+  const [browsingPageResults, setBrowsingPageResults] = useState([]);
+
+  const fetchBrowsingResults = async () => {
+    try {
+      let response = await axios.get(
+        "https://www.cheapshark.com/api/1.0/deals"
+      );
+      console.log(response);
+      setBrowsingPageResults(response.data);
+    } catch (error) {
+      console.warn("Error with fetchBrowsingResults get request", error);
+    }
+  };
+  useEffect(() => {
+    fetchBrowsingResults();
+    /*     setTimeout(() => {
+      setWishListPageLoaded(true);
+    }, 500); */
+  }, []);
+  return (
+    <div>
+      <BrowsingPageResults browsingPageResults={browsingPageResults} />
+    </div>
+  );
 };
 
 export default BrowsingPage;
