d771afe0ae3586fabffa103d742da2533517900a
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-11-03 21:41:54 -0500

    Got a small start setting up the search page.

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,9 +1,33 @@
-import SearchPageResults from "../../components/SearchPageResults/SearchPageResults";
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const SearchPage = ({}) => {
-  return <div></div>;
+  const [searchPageResults, setSearchPageResults] = useState([]);
+
+  const fetchSearchResults = async () => {
+    try {
+      let response = await axios.get(
+        `https://www.cheapshark.com/api/1.0/games?title=${searchInput}`
+      );
+      console.log(response);
+      setSearchPageResults(response.data);
+    } catch (error) {
+      console.warn("Error with fetchsearchResults get request", error);
+    }
+  };
+  useEffect(() => {
+    fetchSearchResults();
+  }, []);
+  return (
+    <div>
+      <div>
+        <input type="text" />
+        <button></button>
+      </div>
+
+      <searchPageResults searchPageResults={searchPageResults} />
+    </div>
+  );
 };
 
 export default SearchPage;
